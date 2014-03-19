caefb0fe7caf3c2bb29ea1b73139ac70afd6d9c5
gwayneson <chebert@spsu.edu>
2014-03-19 15:32:42 -0400

    Rotation & Plugins
    - Added Cannon Rotation
    - Added Plugins (for future use)
    - Added reference to HTML file (for Box2D)

--- README.md
+++
@@ -1,4 +0,0 @@
-CGDD4303-Project
-================
-
-The team project to make a game that helps teach younger children about rocks, minerals, and soil.
--- main.js
+++ main.js
@@ -1,57 +1,21 @@
-/**
- * enchant();
- * Preparation for using enchant.js.
- * (Exporting enchant.js class to global namespace.
- *  ex. enchant.Sprite -> Sprite etc..)
- *
- */
+//Makes enchant.js global namespace
 enchant();
 
-/*
- * window.onload
- *
- * The function which will be executed after loading page.
- * Command in enchant.js such as "new Core();" will cause an error if executed before entire page is loaded.
- *
- */
 window.onload = function(){
-    /**
-     * new Core(width, height)
-     *
-     * Make instance of enchant.Core class. Set window size to 320 x 320
-     */
+
+	//Sets window size of core
     var game = new Core(400, 400);
-
-    /**
-     * Core.fps
-     *
-     * Set fps (frame per second) in this game to 30.
-     */
     game.fps = 30;
-    /**
-     * Core.preload
-     *
-     * You can preload all assets files before starting the game.
-     * Set needed file lists in relative/absolute path for attributes of Core.preload
-     */
+	
+	//Preloads assets for in-game use
     game.preload("Background.png", "Cannon.png", "Bullet.png");
-
-    /**
-     * Core.onload
-     *
-     * game.onload = function(){
-     *     // code
-     * }
-     *
-     * game.addEventListener("load", function(){
-     *     // code
-     * })
-     */
+	
+	//Code for game is set right after it begins. (First scene, starting menu, etc)
     game.onload = function(){
-
+		
         backgroundImage = new Sprite(400, 400);
         cannon = new Sprite(100, 50);
-        bullet = new Sprite(50, 50);
+        var bullet = new Sprite(50, 50);
 
         backgroundImage.image = game.assets["Background.png"];
         cannon.image = game.assets["Cannon.png"];
@@ -63,39 +27,47 @@ window.onload = function(){
         cannon.x = 50;
         cannon.y = 250;
         
-        bullet.x = 100;
-        bullet.y = 0;
-
-        backgroundImage.frame = 0;
-        cannon.frame = 0;
-        bullet.frame = 0;
+        bullet.x = cannon.x;
+        bullet.y = cannon.y;
+        bullet.visible = false;
         
         game.rootScene.addChild(backgroundImage);
+        game.rootScene.addChild(bullet);
         game.rootScene.addChild(cannon);
-      
-
-        /*
-          Add an event to an object:
-
-          EventTarget.addEventListener(event, listener)
-         */         
-        	cannon.addEventListener("touchstart", function(){
-        		bullet.x = 100;
-        		bullet.y = 0;
-            	game.rootScene.addChild(bullet);  
-        	});
-		
-		
-        bullet.addEventListener("enterframe", function(){
-
-            this.x += 10;
-            
-            if(this.x >= 400)
-            {
-            	game.rootScene.removeChild(this);	
-            }
+        
+        //EventTarget.addEventListener(event, listener)      
+        cannon.addEventListener("touchstart", function(){
+        	bullet.x = cannon.x;
+        	bullet.y = cannon.y;
+            bullet.visible = true; 
         });
-
+		
+		cannon.addEventListener("enterframe", function(){
+			if(game.input.up && this.rotation > -90) 
+			{
+				this.rotate(-1);
+			}
+			if(game.input.down && this.rotation < 0)
+			{
+			 	this.rotate(1);	
+			}
+			if(game.input.a)
+			{
+				bullet.x = cannon.x;
+        		bullet.y = cannon.y;
+            	game.rootScene.addChild(bullet);  
+			}
+        });
+        
+        bullet.addEventListener("enterframe", function(){
+        	if(this.visible)
+        	{
+            	this.x += 10;
+            	if(this.x >= 400) {
+            		this.visible = false;	
+            	}        		
+        	}
+        });
     };
 
     game.start();
