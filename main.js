d58ed042a76d5532a7d7c0a5ec278a265512d54e
Erik Swanson <senuskaaf@gmail.com>
2014-03-24 11:58:13 -0400

    Rollback

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ window.onload = function(){
 
     game.fps = 15;
     game.preload("chara1.png");
+
     game.onload = function(){
         bear = new Sprite(32, 32);
 
@@ -16,12 +17,13 @@ window.onload = function(){
         bear.frame = 0;
         game.rootScene.addChild(bear);
 
+
         game.rootScene.addEventListener('touchstart', function(e){
             bear.x = e.localX
 			bear.y = e.localY
         });
-		
-		game.rootScene.addEventListener('touchmove', function(e){
+
+        game.rootScene.addEventListener('touchmove', function(e){
             bear.x = e.localX
 			bear.y = e.localY
         });
