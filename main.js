168c354e9b9985a17e63347d0c11e4ef539bac41
gwayneson <chebert@spsu.edu>
2014-04-02 13:00:57 -0400

    Scene Switch (Final)
    Figured it out. This is the combined code for scene switch (Erik) and
    Cannon (Cameron)

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ window.onload = function(){
 		
 		// Display values for play_button
 		play_button = new Sprite(64, 32);
-		play_button.image = game.assets["button.png"];
+		play_button.image = game.assets["assets/button.png"];
 		play_button.x = 800/2 - 32;
 		play_button.y = 600/2
 		menu_scene.addChild(play_button);
@@ -39,14 +39,43 @@ window.onload = function(){
 		// Define the Play Scene
 		// ----------------------
 		
+		// Local constant for maximum cannon power
+		const MAX_CANNON_POWER = 3;
+		play_scene.backgroundColor = "blue";
+		
 		// Display values for back_button
 		back_button = new Sprite(32, 32);
-		back_button.image = game.assets["button.png"];
+		back_button.image = game.assets["assets/button.png"];
 		back_button.x = 0;
 		back_button.y = 0;
 		play_scene.addChild(back_button);
 		
-		// Input logic for back_button
+		//function(staticOrDynamic, density, friction, restitution, awake)
+        var floor = new PhyBoxSprite(400, 100, enchant.box2d.STATIC_SPRITE, 1.0, 0.5, 0.3, true);
+        floor.image = game.assets["assets/Background.png"];
+        floor.x = 0;
+        floor.y = 300;
+        
+        var cannon = new Sprite(100, 50);
+        cannon.image = game.assets["assets/Cannon.png"];    
+		cannon.x = 50;
+		cannon.y = 250;
+        
+       	play_scene.addChild(floor);
+        play_scene.addChild(cannon);
+        
+        //Binds spacebar to 'a' button
+        game.keybind(32, 'a');
+        
+        //Prevents machine gun cannon
+        var buildingPower = false;
+        var cannonPower = 0.1;
+        
+        play_scene.addEventListener("enterframe", function () {
+        	physicsWorld.step(game.fps);
+        });
+        
+        // Input logic for back_button
 		play_scene.addEventListener('touchstart', function(mousePos){
 			if(mousePos.localX > back_button.x && mousePos.localX < back_button.x + 32)
 			{
@@ -56,34 +85,6 @@ window.onload = function(){
 					game.pushScene(menu_scene);
 				}
 			}
-        });
-		
-		// Local constant for maximum cannon power
-		const MAX_CANNON_POWER = 3;
-		
-		//function(staticOrDynamic, density, friction, restitution, awake)
-        var floor = new PhyBoxSprite(400, 100, enchant.box2d.STATIC_SPRITE, 1.0, 0.5, 0.3, true);
-        floor.image = game.assets["Background.png"];
-        floor.x = 0;
-        floor.y = 300;
-        
-        var cannon = new Sprite(100, 50);
-        cannon.image = game.assets["Cannon.png"];    
-		cannon.x = 50;
-		cannon.y = 250;
-        
-        game.rootScene.addChild(floor);
-        game.rootScene.addChild(cannon);
-        
-        //Binds spacebar to 'a' button
-        game.keybind(32, 'a');
-        
-        //Prevents machine gun cannon
-        var buildingPower = false;
-        var cannonPower = 0.1;
-        
-        game.rootScene.addEventListener("enterframe", function () {
-        	physicsWorld.step(game.fps);
         });
 
         cannon.addEventListener("enterframe", function(){
@@ -97,12 +98,12 @@ window.onload = function(){
         	if(!game.input.a && buildingPower == true)
         	{
         		var drill = new PhyCircleSprite(8, enchant.box2d.DYNAMIC_SPRITE, 1.0, 0.5, 0.2, true);
-       		 	drill.image = game.assets["DrillBall.png"];
+       		 	drill.image = game.assets["assets/DrillBall.png"];
        			drill.x = (Math.cos(this.rotation * 3.14159/180) * (cannon.width / 2)) + (cannon.x + (cannon.width / 2) - (drill.width / 2));
        	 		drill.y = (Math.sin(this.rotation * 3.14159/180) * cannon.height) + (cannon.y + ((cannon.height / 2) - (drill.height / 2)));
         	
          		drill.applyImpulse(new b2Vec2(Math.cos(cannon.rotation * 3.14159/180) * cannonPower, Math.sin(cannon.rotation * 3.14159/180) * cannonPower));
-       		    game.rootScene.addChild(drill);
+       		    play_scene.addChild(drill);
   	    	 	/*
   	    	 		MEANT FOR REMOVING OBJECTS FROM THE GAME
   	    	 		
