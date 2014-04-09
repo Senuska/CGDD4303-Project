3b6120380402f9ec1797309a16154d45a5c0e887
gwayneson <chebert@spsu.edu>
2014-04-09 19:14:49 -0400

    Drilling is IN!
     - Can now successfully drill! Hooray!
     - Animated and correct .png for drill

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ window.onload = function(){
     	var menu_scene = new Scene();
     	var play_scene = new Scene();
     game.fps = 30;
-	game.preload("assets/chara1.png", "assets/button.png", "assets/cannon.png", "assets/drillball.png", "assets/tileset.png");
+	game.preload("assets/chara1.png", "assets/button.png", "assets/cannon.png", "assets/tileset.png", "assets/drill.png");
     game.onload = function(){
 		
 		// ----------------------
@@ -18,8 +18,8 @@ window.onload = function(){
 		// Display values for play_button
 		play_button = new Sprite(64, 32);
 		play_button.image = game.assets["assets/button.png"];
-		play_button.x = 800/2 - 32;
-		play_button.y = 600/2
+		play_button.x = game.width/2 - 32;
+		play_button.y = game.height/2
 		menu_scene.addChild(play_button);
 		
 		// Input logic for play_button
@@ -37,49 +37,39 @@ window.onload = function(){
 		// ----------------------
 		// Define the Play Scene
 		// ----------------------
+
+		/* Physics objects follow this format: function(staticOrDynamic, density, friction, restitution, awake)
+           staticOrDynamic: Does this object move?
+           Density:			Heaviness of object
+           Friction:		Higher friction produces less slipperiness
+           Restitution:		Bounceback of collisions
+           Awake:			Object recognizes forces
+        */
+        
+		// Creates the test floor as a group
+		// To-do: Place this inside of one method or have a better way to generate terrain
+		var floorGroup = new Group();
 		
-		// Creates the floor of the level (use map editor: http://enchantjs.com/resource/the-map-editor/)
-		var mapFloor = new Map(16, 16);
-        mapFloor.image = game.assets["assets/tileset.png"];
-        mapFloor.y = 300;
-        mapFloor.loadData(
-            [
-				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-				[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-				[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-				[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],	
-			]
-		);
-		
-		// Creates the test wall of the level
-		var mapWall = new Map(16, 16);
-		
-		// Store it in an array for changing the map (game won't recognize map changes)
-		mapWall.values = [
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 2],
-			];
-		mapWall.image = game.assets["assets/tileset.png"];
-		mapWall.x = 400;
-		mapWall.y = 16;
-		mapWall.loadData(mapWall.values);
-		
+		for(var i = 0; i < 20; i++)
+		{
+        	var block = new PhyBoxSprite(16, 16, enchant.box2d.STATIC_SPRITE, 1.0, 0.5, 0.01, true);
+        	block.image = game.assets["assets/tileset.png"];
+        	block.frame = 0;
+        	block.position = { x: i*16, y: 300 };
+      		floorGroup.addChild(block);
+        }
+        
+        for(var i = 1; i < 4; i++)
+        {
+        	for(var j = 0; j < 20; j++)
+        	{
+        		var block = new PhyBoxSprite(16, 16, enchant.box2d.STATIC_SPRITE, 1.0, 0.5, 0.25, true);
+        		block.image = game.assets["assets/tileset.png"];
+        		block.frame = 1;
+        		block.position = {x: j*16, y: 300 + (i*16)};
+        		floorGroup.addChild(block);
+        	}	
+        }
 		// Local constant for maximum cannon power
 		const MAX_CANNON_POWER = 3;
 		const CANNON_TEXT = "Cannon Power: ";
@@ -97,10 +87,8 @@ window.onload = function(){
 		back_button.image = game.assets["assets/button.png"];
 		back_button.x = 0;
 		back_button.y = 0;
-		
-      	// Physics objects follow this format: function(staticOrDynamic, density, friction, restitution, awake)
-        // var floor = new PhyBoxSprite(400, 100, enchant.box2d.STATIC_SPRITE, 1.0, 0.5, 0.3, true);
 
+		// Various values for cannon
         var cannon = new Sprite(100, 50);
         cannon.image = game.assets["assets/cannon.png"];    
 		cannon.x = 50;
@@ -137,41 +125,27 @@ window.onload = function(){
         	
         	if(!game.input.a && cannon.buildingPower == true)
         	{
-        		var drill = new PhyCircleSprite(8, enchant.box2d.DYNAMIC_SPRITE, 1.0, 0.5, 0.2, true);
-       		 	drill.image = game.assets["assets/drillball.png"];
+        		// Bug: Drill spawns correctly rotated, but then snaps back to straight orientation
+        		var drill = new PhyBoxSprite(16, 16, enchant.box2d.DYNAMIC_SPRITE, 1.0, 0.5, 0.3, true);
+       		 	drill.image = game.assets["assets/drill.png"];
+       		 	drill.frame = 0;
+       		 	drill.rotation = cannon.rotation;
        			drill.x = (Math.cos(this.rotation * 3.14159/180) * (cannon.width / 2)) + (cannon.x + (cannon.width / 2) - (drill.width / 2));
        	 		drill.y = (Math.sin(this.rotation * 3.14159/180) * cannon.height) + (cannon.y + ((cannon.height / 2) - (drill.height / 2)));
         	
          		drill.applyImpulse(new b2Vec2(Math.cos(cannon.rotation * 3.14159/180) * cannon.power, Math.sin(cannon.rotation * 3.14159/180) * cannon.power));
-       		    play_scene.addChild(drill);
-  	    	 	drill.addEventListener("enterframe", function(){	
-  	    	 		if (drill.intersect(mapWall)) 
-  	    	 		{
-  	    	 			/*
-  	    	 				Bug: Only removes on first shot.
-  	    	 				Bug: Crashes if you hit corners.
-  	    	 				Bug: Sometimes removes incorrect blocks or not at all.
-  	    	 			*/
-  	    	 			
-					// Remove block found at the intersection site
-					var hitY = ((mapWall.y - drill.y) / 16).toFixed(0);
-					var hitX = ((mapWall.x - drill.x) / 16).toFixed(0);
-					
-					if (hitY < 0) hitY = -hitY;
-					if (hitX < 0) hitX = -hitX;
-					
-						mapWall.values[hitY][hitX] = (-1);
-  	    	 			
-  	    	 		// Loads changed array data into map for drawing
-						mapWall.loadData(mapWall.values);
-
-					// If drill velocity drops to a certain speed, remove the drill
-					 if (drill.velocity.x <= 0.5 && drill.velocity.y <= 0.5) drill.destroy();
-  	    	 		}
+  	    	 	drill.addEventListener("enterframe", function(){
+  	    	 		
+  	    	 		// To-do: Add conditions to remove drill. ie: Low velocity or cargo met.
+  	    	 		this.frame = this.age % 6;
+  	    	 		drill.contact(function (object) {
+						object.destroy();
+					});
            	   	});
            	   		
            	   	cannon.buildingPower = false;
            	   	cannon.power = 0.0;
+           	   	play_scene.addChild(drill);
         	}
         	
         	if(game.input.up && this.rotation > -90) this.rotate(-1);					
@@ -180,9 +154,8 @@ window.onload = function(){
         
         // Adds all items to the play_scene
         play_scene.addChild(back_button);
-        play_scene.addChild(mapFloor);
-        play_scene.addChild(mapWall);
         play_scene.addChild(cannon);
+        play_scene.addChild(floorGroup);
         play_scene.addChild(powerLabel);
     };
     game.start();
