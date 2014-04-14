0d2ca0d6a8abb9261e8a4ebf2f6e5167878104f0
gwayneson <chebert@spsu.edu>
2014-04-14 15:33:46 -0400

    Drilling v3.0
    - Drilling now correctly functions
    - Code improvement (efficiency)
    - Added nifty to do list to keep up with tasks

--- main.js
+++ main.js
@@ -32,41 +32,58 @@ window.onload = function(){
 		// ----------------------
 
 		/* Physics objects follow this format: function(staticOrDynamic, density, friction, restitution, awake)
-           staticOrDynamic: Does this object move?
+           staticOrDynamic: Move or stationary
            Density:			Heaviness of object
            Friction:		Higher friction produces less slipperiness
-           Restitution:		Bounceback of collisions
+           Restitution:		Rebound amount
            Awake:			Object recognizes forces
         */
         
-		// Creates the test floor as a group
-		// To-do: Place this inside of one method or have a better way to generate terrain
-		var floorGroup = new Group();
-		
-		for(var i = 0; i < 20; i++)
+        /*
+           Cameron's To-Do List of Stuff To Do:
+           [x] Update physics boxes along with images for blocks
+           [ ] Level Design (at least one level)
+           [ ] Rotate drill correctly and remove 'snapback bug'
+           [ ] Add conditions to remove drill. ie: Low velocity or cargo met.
+           [x] Prevent drill movement after it reaches center screen (while shooting, stop movement)
+           [ ] HUD update (make it look better, maybe bars for power and age)
+           [x] Prevent multiple drill shots
+           [ ] Update cannon image (Make it LOOK BETTA)
+           [ ] Add interface for scrolling through map before firing (waypoints?)
+        */
+        
+		// Creates the stage group to hold level blocks and cannon
+		var stage = new Group();
+		stage.x = 0;
+		stage.y = 0;
+		stage.STAGE_ORIGIN = 0;
+
+		for(var i = 0; i < 10; i++)
 		{
-        	var block = new PhyBoxSprite(16, 16, enchant.box2d.STATIC_SPRITE, 1.0, 0.5, 0.01, true);
-        	block.image = game.assets["assets/tileset.png"];
-        	block.frame = 0;
-        	block.position = { x: i*16, y: 300 };
-      		floorGroup.addChild(block);
+			for(var j = 0; j < 20; j++)
+			{
+				if(i == 0) 
+				{
+					var block = new PhyBoxSprite(16, 16, enchant.box2d.STATIC_SPRITE, 1.0, 0.5, 0.01, true);
+					block.frame = 0;
+				}
+				else
+				{
+				 	var block = new PhyBoxSprite(16, 16, enchant.box2d.STATIC_SPRITE, 1.0, 0.5, 0.25, true);
+				 	block.frame = 1;	
+				}
+
+        		block.image = game.assets["assets/tileset.png"];
+        		block.position = {x: (j*16), y: ((i*16) + 300)};
+      			stage.addChild(block);				
+			}
         }
         
-        for(var i = 1; i < 4; i++)
-        {
-        	for(var j = 0; j < 20; j++)
-        	{
-        		var block = new PhyBoxSprite(16, 16, enchant.box2d.STATIC_SPRITE, 1.0, 0.5, 0.25, true);
-        		block.image = game.assets["assets/tileset.png"];
-        		block.frame = 1;
-        		block.position = {x: j*16, y: 300 + (i*16)};
-        		floorGroup.addChild(block);
-        	}	
-        }
 		// Local constant for maximum cannon power
 		const MAX_CANNON_POWER = 3;
 		const CANNON_TEXT = "Cannon Power: ";
 		const AMMO_TEXT = "Drills Remaining: ";
+		const CARGO_TEXT = "Total Cargo: ";
 		play_scene.backgroundColor = "blue";
 		
 		// Binds spacebar to a-button
@@ -89,35 +106,37 @@ window.onload = function(){
 		cannon.y = 250; 
 		cannon.power = 0.0;     		//Current cannon launch power
 		cannon.buildingPower = false;	// Prevents machine gun cannon
+		cannon.canFire = true;
 		cannon.ammo = 3;
+		cannon.cargo = 0;
         
         // Logic for physics steps
         play_scene.addEventListener("enterframe", function () {
         	physicsWorld.step(game.fps);
-        	hudLabel.text = (CANNON_TEXT + cannon.power.toFixed(1) + "\n" + AMMO_TEXT + cannon.ammo);
+        	hudLabel.text = (CANNON_TEXT + cannon.power.toFixed(1) + " | " + AMMO_TEXT + cannon.ammo + " |                  	   " + CARGO_TEXT + cannon.cargo + "/100");
         });
         
         // Logic for back_button
-		play_scene.addEventListener('touchstart', function(){
+		back_button.addEventListener('touchstart', function(){
 			game.replaceScene(menu_scene);
         });
 
 		// Logic for cannon
         cannon.addEventListener("enterframe", function(){
-        	if(game.input.a && cannon.buildingPower == false) cannon.buildingPower = true;
-        	else if(game.input.a && cannon.buildingPower == true)
+        	if(game.input.a && !cannon.buildingPower && cannon.canFire) cannon.buildingPower = true;
+        	else if(game.input.a && cannon.buildingPower)
         	{
 				cannon.power += 0.1;
 				if(cannon.power > MAX_CANNON_POWER) cannon.power = MAX_CANNON_POWER;
         	}
         	
-        	if(!game.input.a && cannon.buildingPower == true)
+        	if(!game.input.a && cannon.buildingPower && cannon.canFire)
         	{
-        		// Bug: Drill spawns correctly rotated, but then snaps back to straight orientation
-        		//var drill = new PhyBoxSprite(16, 16, enchant.box2d.DYNAMIC_SPRITE, 1.0, 0.5, 0.3, true);
+        		cannon.canFire = false;
         		
         		if(cannon.ammo > 0)
         		{
+        			cannon.ammo -= 1;
         			var drill = new PhyCircleSprite(8, enchant.box2d.DYNAMIC_SPRITE, 1.0, 0.5, 0.3, true);
        		 		drill.image = game.assets["assets/drill.png"];
        		 		drill.frame = 0;
@@ -128,29 +147,41 @@ window.onload = function(){
        		  		drill.applyImpulse(new b2Vec2(Math.cos(cannon.rotation * 3.14159/180) * cannon.power, Math.sin(cannon.rotation * 3.14159/180) * cannon.power));
   	    		 	drill.addEventListener("enterframe", function(){
   	    	 		
-  	    	 			// To-do: Add conditions to remove drill. ie: Low velocity or cargo met.
   	    	 			this.frame = this.age % 6;
   	    	 			drill.contact(function (object) {
+  	    	 				if(object.frame == 1) cannon.cargo++;
 							object.destroy();
 						});
-           	   		});
-           	   		
-           	   		cannon.ammo -= 1;
-           	   		play_scene.addChild(drill);        			
-        		}
+						
+						if((game.width / 2 - drill.x) <= 0) stage.x = (game.width / 2 - drill.x);
+						if((game.height / 2 - drill.y) <= 0) stage.y = (game.height / 2 - drill.y);
 
+						//Possibly edit this to add velocity constraint
+						if(cannon.cargo >= 100 || this.age >= 100) 
+						{
+							drill.destroy();
+							cannon.canFire = true;
+							stage.x = stage.STAGE_ORIGIN;
+							stage.y = stage.STAGE_ORIGIN;
+						}
+           	   		});
+
+           	   		stage.addChild(drill);        			
+        		}
+        		
+        		cannon.power = 0.0;
            	   	cannon.buildingPower = false;
-           	   	cannon.power = 0.0;
         	}
         	
         	if(game.input.up && this.rotation > -90) this.rotate(-1);					
 			if(game.input.down && this.rotation < 0) this.rotate(1);
         });
         
+        stage.addChild(cannon);
+        
         // Adds all items to the play_scene
         play_scene.addChild(back_button);
-        play_scene.addChild(cannon);
-        play_scene.addChild(floorGroup);
+        play_scene.addChild(stage);
         play_scene.addChild(hudLabel);
     };
     game.start();
